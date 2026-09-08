2adf0fd70ca6a24f0c1639f59bbd7b322a630c94
ffxupdate25k <ffxupdate25k@gmail.com>
2026-09-08 22:46:33 +0100

    Delete server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const fs = require("fs");
+const fs = require("fs")
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const Database = require("better-sqlite3");
